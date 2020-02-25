4e529bc8b06872433ce04aea671a5109d7bf341f
Milad Latif <mlatif01@dcs.bbk.ac.uk>
2020-02-25 11:32:40 +0000

    section 2

---
+++ 2 - Movie Seat Booking/script.js
@@ -0,0 +1,74 @@
+// --- DOM Elements and Field Variables---
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelected = document.getElementById('movie');
+
+let ticketPrice = +movieSelected.value;
+
+// Called on startup
+populateUI();
+
+// --- Functions ---
+// Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+// Update total and count
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+  const selectedSeatsCount = selectedSeats.length;
+
+  count.innerHTML = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// Get data from local storage and populate UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+  if (selectedMovieIndex !== null) {
+    movieSelected.selectedIndex = selectedMovieIndex;
+  }
+}
+
+// --- Event Handlers --- 
+
+// Movie select event
+movieSelected.addEventListener('change', e => {
+  ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, ticketPrice);
+  updateSelectedCount();
+});
+
+// Seat click event
+container.addEventListener('click', e => {
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+  ) {
+    e.target.classList.toggle('selected');
+    updateSelectedCount();
+  }
+});
+
+// Initial count and total set
+updateSelectedCount();
